94a1a61f399c5cd25f021d852176feb5ed28ae47
Joel Eguiza <joeleg96@gmail.com>
2025-04-28 14:51:01 -0600

    finished js to allow for answer to minimize/expand based on user click

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
-$(".plus-icon").on("click", function(event){
-    console.log(event.target.id);
-    switch (key) {
-        case value:
-            
-            break;
+$(".icon").on("click", function(event){
+    var questionID = event.target.id;
+    console.log(questionID);
+    switch (questionID) {
+        case "one-plus":
+            $("#" +"one-plus").toggle(".hide");
+            $("#" +"one-minus").toggle(".hide");
+            $("#" +"one-p").toggle(".hide");
+        break;
+
+        case "one-minus":
+            $("#" +"one-plus").toggle(".hide");
+            $("#" +"one-minus").toggle(".hide");
+            $("#" +"one-p").toggle(".hide");
+        break;
+
+        case "two-plus":
+            $("#" +"two-plus").toggle(".hide");
+            $("#" +"two-minus").toggle(".hide");
+            $("#" +"two-p").toggle(".hide");
+        break;
+
+        case "two-minus":
+            $("#" +"two-plus").toggle(".hide");
+            $("#" +"two-minus").toggle(".hide");
+            $("#" +"two-p").toggle(".hide");
+        break;
+
+        case "three-plus":
+            $("#" +"three-plus").toggle(".hide");
+            $("#" +"three-minus").toggle(".hide");
+            $("#" +"three-p").toggle(".hide");
+        break;
+
+        case "three-minus":
+            $("#" +"three-plus").toggle(".hide");
+            $("#" +"three-minus").toggle(".hide");
+            $("#" +"three-p").toggle(".hide");
+        break;
+
+        case "four-plus":
+            $("#" +"four-plus").toggle(".hide");
+            $("#" +"four-minus").toggle(".hide");
+            $("#" +"four-p").toggle(".hide");
+        break;
+
+        case "four-minus":
+            $("#" +"four-plus").toggle(".hide");
+            $("#" +"four-minus").toggle(".hide");
+            $("#" +"four-p").toggle(".hide");
+        break;
     
         default:
+            console.log(questionID);
             break;
     }
 })
+
