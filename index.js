385fad0dd7e0f6f0fb1a3d25416963cd7e88142a
Joel Eguiza <joeleg96@gmail.com>
2025-04-28 13:05:59 -0600

    Inital commit

---
+++ index.js
@@ -0,0 +1,11 @@
+$(".plus-icon").on("click", function(event){
+    console.log(event.target.id);
+    switch (key) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+})
